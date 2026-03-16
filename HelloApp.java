6e123576615f21a80536df636bc67f3a94610d93
Deb Jyoti Ghosh <debjyoti08adamas@gmail.com>
2026-03-16 15:07:52 +0530

    Add HelloApp entry point on main

---
+++ HelloApp.java
@@ -0,0 +1,8 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello App");
+
+        // UC1 entry point; add future UC calls here.
+        Uc1.main(args);
+    }
+}
