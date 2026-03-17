7d4a42dc53d8940ebcd7219319a5a9a3ad282cbe
Deb Jyoti Ghosh <debjyoti08adamas@gmail.com>
2026-03-17 08:46:45 +0530

    UC5: use enhanced for-loop for multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,18 +1,15 @@
-import java.util.Scanner;
-
 public class HelloApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter names: ");
-        String input = sc.nextLine().trim();
-
-        if (input.isEmpty()) {
-            System.out.println("Hello World");
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello " + input);
+            System.out.print("Hello ");
+            
+            for (String name : args) {
+                System.out.print(name + " ");
+            }
+            
+            System.out.println();
         }
-
-        sc.close();
     }
 }
